5acbffe3e8dfcb533960beca8f40d4cf025079b9
Gabriel Quaresma <fulco02@gmail.com>
2021-06-19 17:27:04 -0300

    port

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ app.use(express.json());
 app.use("/books", books);
 app.use("/purchase", purchase);
 
-app.listen(process.env.APP_URL || 3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log(app_url);
 });
